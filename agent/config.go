457d2bf411da6e2f8378e4dc9cffe8020fa11c08
GPTOT <143704223+GPTOT@users.noreply.github.com>
2021-04-26 13:26:09 +0000

    statistic

--- agent/config.go
+++ agent/config.go
@@ -73,4 +73,4 @@ func (t Topic) String() string {
 
 // Returns the name of the topic to consume from.
 // If the topic direction is `Push` then consume from the source topic.
-// If the topic direction is `
+// If the topic direction is `Pull` then consume from the destinatio
