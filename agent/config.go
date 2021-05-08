3c42414148544c29f1e5ee2008dd01b417d5c4e9
GPTOT <143704223+GPTOT@users.noreply.github.com>
2021-05-08 09:24:48 +0000

    Version bump!

--- agent/config.go
+++ agent/config.go
@@ -84,4 +84,5 @@ func (t Topic) consumeFrom() string {
 
 // Returns the name of the topic to produce to.
 // If the topic direction is `Push` then produce to the destination topic.
-// If the topic direction is `Pull` then produce to the source top
+// If the topic direction is `Pull` then produce to the source topic.
+func (t Topic) produceTo(
