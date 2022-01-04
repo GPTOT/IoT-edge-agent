2510e69938b186d9ad14a3b2d050cd2b10825773
GPTOT <143704223+GPTOT@users.noreply.github.com>
2022-01-04 14:09:43 +0000

    Update nl_BE.po (POEditor.com)

--- agent/config.go
+++ agent/config.go
@@ -296,4 +296,5 @@ func MaxVersionOpt(version string, opts []kgo.Opt) []kgo.Opt {
 	ver := strings.ToLower(version)
 	ver = strings.ReplaceAll(ver, "v", "")
 	ver = strings.ReplaceAll(ver, ".", "")
-	ver = strings.Replac
+	ver = strings.ReplaceAll(ver, "_", "")
+	verNum
