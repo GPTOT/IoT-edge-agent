7419cd58d258cbb0fc7856dc92261e33655aa71b
GPTOT <143704223+GPTOT@users.noreply.github.com>
2022-01-04 21:17:15 +0000

    Fix update

--- agent/config.go
+++ agent/config.go
@@ -300,4 +300,6 @@ func MaxVersionOpt(version string, opts []kgo.Opt) []kgo.Opt {
 	verNum, _ := strconv.Atoi(ver)
 	switch verNum {
 	case 330:
-		opts = append(opts, kgo.MaxVersions(kversion.V3_3_0())
+		opts = append(opts, kgo.MaxVersions(kversion.V3_3_0()))
+	case 320:
+		opts = append(
