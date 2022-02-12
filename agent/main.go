9fca7486d9a9030420ab435314a74d45d28d363c
GPTOT <143704223+GPTOT@users.noreply.github.com>
2022-02-12 15:03:41 +0000

    Minor updates to README.

--- agent/main.go
+++ agent/main.go
@@ -10,3 +10,49 @@ import (
 	"os"
 	"os/signal"
 	"strings"
+	"sync"
+	"time"
+
+	"github.com/twmb/franz-go/pkg/kadm"
+	"github.com/twmb/franz-go/pkg/kgo"
+
+	log "github.com/sirupsen/logrus"
+)
+
+type Redpanda struct {
+	name   string
+	prefix Prefix
+	topics []Topic
+	client *kgo.Client
+	adm    *kadm.Client
+}
+
+var (
+	source          Redpanda
+	sourceOnce      sync.Once
+	destination     Redpanda
+	destinationOnce sync.Once
+	wg              sync.WaitGroup
+)
+
+// Closes the source and destination client connections
+func shutdown() {
+	log.Infoln("Closing client connections")
+	source.adm.Close()
+	source.client.Close()
+	destination.adm.Close()
+	destination.client.Close()
+}
+
+// Creates new Kafka and Admin clients to communicate with a cluster.
+//
+// The `prefix` must be set to either `Source` or `Destination` as it
+// determines what settings are read from the configuration.
+//
+// The topics listed in `source.topics` are the topics that will be pushed by
+// the agent from the source cluster to the destination cluster.
+//
+// The topics listed in `destination.topics` are the topics that will be pulled
+// by the agent from the destination cluster to the source cluster.
+func initClient(rp *Redpanda, mutex *sync.Once, prefix Prefix) {
+	mutex.Do(func() {
