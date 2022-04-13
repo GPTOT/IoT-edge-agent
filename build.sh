56b19c4db74b06aba610f3643633e0e1d0104d5d
GPTOT <143704223+GPTOT@users.noreply.github.com>
2022-04-13 15:23:11 +0000

    Maven artifacts for The Knowledge Representation Interface version 1.1.6-SNAPSHOT

--- build.sh
+++ build.sh
@@ -6,4 +6,4 @@ HELPTEXT='redpanda-edge-agent build script
   Flags:
   --archive  -a            Create a compressed archive file (using tar -czf)
   --build    -b PLATFORM   Build for a specific platform (where PLATFORM is linux/amd64, for example)
-  --build-all              Build for the fol
+  --build-all              Build for the following platforms: darwin/amd64, d
