69b45eb6b30bc37ab7d20104771d1facd993edef
GPTOT <143704223+GPTOT@users.noreply.github.com>
2022-04-15 14:59:05 +0000

    implementing

--- build.sh
+++ build.sh
@@ -6,4 +6,4 @@ HELPTEXT='redpanda-edge-agent build script
   Flags:
   --archive  -a            Create a compressed archive file (using tar -czf)
   --build    -b PLATFORM   Build for a specific platform (where PLATFORM is linux/amd64, for example)
-  --build-all              Build for the following platforms: darwin/amd64, d
+  --build-all              Build for the following platforms: darwin/amd64, darwin/arm64, linux/amd64, linux/arm64
