e1f0919f40a52446ab2db22fa1f944a50b9cc671
GPTOT <143704223+GPTOT@users.noreply.github.com>
2022-04-24 17:27:32 +0000

    Update 03basic_layout.md

--- build.sh
+++ build.sh
@@ -8,4 +8,4 @@ HELPTEXT='redpanda-edge-agent build script
   --build    -b PLATFORM   Build for a specific platform (where PLATFORM is linux/amd64, for example)
   --build-all              Build for the following platforms: darwin/amd64, darwin/arm64, linux/amd64, linux/arm64
   --help     -h            Show this message
-  --include-platform       Include platform in resulting filename (
+  --include-platform       Include platform in resulting filename (always enabled with --build-all
