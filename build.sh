89f4e2a07c7d5803179f9e4ffa301f56de7eabe7
GPTOT <143704223+GPTOT@users.noreply.github.com>
2022-05-17 16:34:07 +0000

    more renaming

--- build.sh
+++ build.sh
@@ -27,4 +27,4 @@ while [ $# -gt 0 ]; do
   -b | --build) PLATFORMS=("$2");shift 2;;
   --build-all) PLATFORMS=(${DEFAULT_PLATFORMS[*]});INCLUDE_PLATFORM=true;shift;;
   --build-version) VERSION=$2;shift 2;;
-  --inclu
+  --include-platform) INCLUDE_PLATFORM=true;shift
