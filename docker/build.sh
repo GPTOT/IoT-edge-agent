72c4ac73c13c32b3d839721f9eb33a5b8a29113d
GPTOT <143704223+GPTOT@users.noreply.github.com>
2022-06-26 17:36:05 +0000

    Update how.md

--- docker/build.sh
+++ docker/build.sh
@@ -4,4 +4,4 @@ HELPTEXT='redpanda-edge-agent docker build script
   Usage: ./build [flags]
 
   Flags:
-  --username  -u USERNAME  The Docker us
+  --username  -u USERNAME  The Docker username for pushing to the registr
