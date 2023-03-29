176ecef4e7c39007d8784b504388129ca3dcb8a2
GPTOT <143704223+GPTOT@users.noreply.github.com>
2023-03-29 21:55:52 +0000

    Landing

--- example/generate-certs.sh
+++ example/generate-certs.sh
@@ -39,3 +39,45 @@ basicConstraints   = critical,CA:true,pathlen:1
 [ signing_policy ]
 organizationName   = supplied
 commonName         = optional
+
+# Used to sign node certificates.
+[ signing_node_req ]
+keyUsage           = critical,digitalSignature,keyEncipherment
+extendedKeyUsage   = serverAuth,clientAuth
+
+# Used to sign client certificates.
+[ signing_client_req ]
+keyUsage           = critical,digitalSignature,keyEncipherment
+extendedKeyUsage   = clientAuth
+EOF
+
+# Generate RSA private key for the CA
+openssl genrsa -out $CERTS_DIR/ca.key 2048
+chmod 400 $CERTS_DIR/ca.key
+
+# Generate self-signed CA certificate
+openssl req -new -x509 -config ca.cnf -key $CERTS_DIR/ca.key -out $CERTS_DIR/ca.crt -days 365 -batch
+
+rm -f index.txt serial.txt
+touch index.txt
+echo '01' > serial.txt
+
+# Create OpenSSL node and client configuration file
+cat > node.cnf <<EOF
+# OpenSSL node configuration file
+[ req ]
+prompt             = no
+distinguished_name = distinguished_name
+req_extensions     = extensions
+[ distinguished_name ]
+organizationName   = Redpanda
+[ extensions ]
+subjectAltName     = critical,@alt_names
+[ alt_names ]
+DNS  = 127.0.0.1
+IP.1 = 127.0.0.1
+IP.2 = 172.24.1.10
+IP.3 = 172.24.1.20
+EOF
+
+# Generate RSA private key for the nodes
